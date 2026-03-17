ace1dbc1de9fdcee0ed8c42b87bd5b0bb7317c43
jr-dev <125911752+juan-erre@users.noreply.github.com>
2026-03-17 15:08:39 +0100

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,15 +1,2 @@
-// ROMPECABEZAS CREADO CON JQUERY
-
-// Instruccion para seleccionar una imagen en miniatura (al hacer click sobre ella, con multitud de posibilidades) y colocarla en la zona de imagen grande seleccionada (al hacer click sobre ella, con multitud de posibilidades) 
-// Explicacion: Captura el primer click sobre una imagen de la clase .imagenes_pequeñas, nos guarda en una variable su src y despues nos captura el segundo click sobre una imagen de la clase .imagen_grande. Aplica un efecto para quitar la imagen que hay + cambia el valor src segun el valor de la variable + aparece la nueva imagen con un efecto
-$("body").on("click", ".imagen_pequeña", function(){
-    let seleccionada = $(this).attr("src");
-    $("body").on("click", ".imagen_grande", function() { 
-	    $(this).fadeOut(500, function(){
-        	$(this).attr("src", seleccionada).fadeIn(500);
-    	});
-	});
-});
-
 // Actualiza el año del footer
 document.getElementById("year").textContent = new Date().getFullYear();
